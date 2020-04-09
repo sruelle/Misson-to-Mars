6067f286279b6da667c62dc0ec83d9d6620f2d8e
Stacey Ruelle <srue23@gmail.com>
2020-04-09 18:25:31 -0500

    Fixing Flask

--- app.py
+++ app.py
@@ -7,19 +7,19 @@ import scrape_mars
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_facts"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_db"
 mongo = PyMongo(app)
 
 # Default Route: Retrive a record from Mongo db mars_facts db and display
 @app.route("/")
 def index():
-    mars_info = mongo.db.mars_facts.find_one()
-    return render_template("index.html", mars_record=mars_info)
+    mars_info = mongo.db.mars.find_one()
+    return render_template("index.html", mars=mars_info)
 
 # Scrape Route: execute scrape() function in screpe_mars.py file, store the returned dictionary in Mongo db
 @app.route("/scrape")
 def scrape():
-    mars_info = mongo.db.mars_facts
+    mars_info = mongo.db.mars
     mars_record = scrape_mars.scrape()
     mars_info.update({}, mars_record, upsert=True)
     return redirect("/", code=302)
